2bd66563aef34bdef4e41e68954d986bf9dd8830
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 15:06:42 +0000

    Final commit

--- m_interest_calculator.py
+++ m_interest_calculator.py
@@ -4,4 +4,20 @@
 # show to the user
 """
 
+def main(): 
+    print("This is a monthly payment laon calculator")
 
+    principal = float(input("Input the loan amount: "))
+    apr = input("Input the anual interest rate: ")
+    years = int(input("Input the amount of years: "))
+
+    monthly_interest_rate = int(apr)/1200
+    number_of_months = years * 12
+
+    monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-number_of_months))
+
+    print("The monthly payment for this loan is: %.2f" %monthly_payment)
+
+    print("")
+while True:
+    main()
